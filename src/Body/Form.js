633ccfdc9f2af8877332fbe6882d76de24133fd4
VendettaTVV <vvtkachukvv@gmail.com>
2023-04-21 20:59:22 +0300

    HW3

--- src/Body/Form.js
+++ src/Body/Form.js
@@ -1,9 +1,19 @@
+import { useState } from 'react';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+import 'react-datepicker/dist/react-datepicker.css';
+
 
 function FormComponent({ show, handleClose }) {
+
+    const [startDateFrom, setStartDateFrom] = useState(new Date());
+    const [startDateTo, setStartDateTo] = useState(new Date());
+    const dateFormat = "dd-MM-yyyy";
+
     const languages = [
         { label: 'English', code: 'en' },
         { label: 'Russian', code: 'ru' },
@@ -11,8 +21,22 @@ function FormComponent({ show, handleClose }) {
         { label: 'French', code: 'fr' },
         { label: 'Spanish', code: 'es' },
     ];
-    function ToCapitalaze (str) {
+    function ToCapitalaze(str) {
         return str[0].toUpperCase() + str.substring(1);
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        const data = {
+            q: event.target.q.value,
+            from: moment(event.target.from.value, dateFormat).format("yyyy-MM-DDT00:00:00.000"),
+            to: moment(event.target.to.value, dateFormat).format("yyyy-MM-DDT23:59:59.999"),
+            language: event.target.language.value,
+            searchIn: [...event.target.searchIn].filter(input => input.checked).map(input => input.value).join(','),
+        };
+        console.log(data);
+    
     }
     return (
         <Offcanvas show={show} onHide={handleClose}>
@@ -20,10 +44,10 @@ function FormComponent({ show, handleClose }) {
                 <Offcanvas.Title>Search news</Offcanvas.Title>
             </Offcanvas.Header>
             <Offcanvas.Body>
-                <Form>
+                <Form onSubmit={handleSubmit}>
                     <Form.Group className="mb-3">
                         <Form.Label>Keywords</Form.Label>
-                        <Form.Control type="text" placeholder="Enter keywords or phrases" />
+                        <Form.Control type="text" name='q' placeholder="Enter keywords or phrases" />
                         <Form.Text className="text-muted">
                             Advanced search is supported here
                         </Form.Text>
@@ -33,7 +57,8 @@ function FormComponent({ show, handleClose }) {
                             <Form.Check
                                 label={ToCapitalaze(type)}
                                 name="searchIn"
-                                type='checkbox'
+                                type="checkbox"
+                                value={type}
                                 id={`inline-${type}-1`}
                             />
                         </div>
@@ -41,16 +66,28 @@ function FormComponent({ show, handleClose }) {
                     <Form.Group className="mb-3">
                         <Form.Label>From - To</Form.Label>
                         <InputGroup className="mb-3">
-                            <Form.Control aria-label="from" />
-                            <Form.Control aria-label="to" />
+                            <DatePicker
+                                className="form-control"
+                                selected={startDateFrom}
+                                onChange={(date) => setStartDateFrom(date)}
+                                name="from"
+                                dateFormat={dateFormat}
+                            />
+                            <DatePicker
+                                className="form-control"
+                                selected={startDateTo}
+                                onChange={(date) => setStartDateTo(date)}
+                                name="to"
+                                dateFormat={dateFormat}
+                            />
                         </InputGroup>
                     </Form.Group>
                     <Form.Group className="mb-3">
                         <Form.Label>Select language</Form.Label>
-                        <Form.Select>
-                           {languages.map((lang) => (
-                            <option>{lang.label}</option>
-                           ))}
+                        <Form.Select name='language'>
+                            {languages.map((lang) => (
+                                <option key={lang.code} value={lang.code}>{lang.label}</option>
+                            ))}
                         </Form.Select>
                     </Form.Group>
                     <Button variant="primary" type="submit" className='w-100'>
--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,26 +1,21 @@
 
 import Pagination from 'react-bootstrap/Pagination';
+import { useState } from 'react';
+
+
 
 function PaginationComponent() {
-  return (
-    <Pagination className='mt-4 justify-content-center'>
-      <Pagination.First />
-      <Pagination.Prev />
-      <Pagination.Item>{1}</Pagination.Item>
-      <Pagination.Ellipsis />
 
-      <Pagination.Item>{10}</Pagination.Item>
-      <Pagination.Item>{11}</Pagination.Item>
-      <Pagination.Item active>{12}</Pagination.Item>
-      <Pagination.Item>{13}</Pagination.Item>
-      <Pagination.Item disabled>{14}</Pagination.Item>
+    const [page, setPage] = useState(1);
 
-      <Pagination.Ellipsis />
-      <Pagination.Item>{20}</Pagination.Item>
-      <Pagination.Next />
-      <Pagination.Last />
-    </Pagination>
-  );
+
+    return (
+        <Pagination className='mt-4 justify-content-center'>
+            <Pagination.Prev onClick={() => setPage(page - 1)} disabled={page === 1} />
+            <Pagination.Item disabled>{page} / 10</Pagination.Item>
+            <Pagination.Next onClick={() => setPage(page + 1)} disabled={page === 10}  />
+        </Pagination>
+    );
 }
 
 export default PaginationComponent;
